f7b20ce0af0579c62e3247285217dc06be637ccf
nicodenelliGH <ndm9412@gmail.com>
2023-03-07 14:58:33 -0500

    Project-1-TicTacToe

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ let boxes = Array(9).fill(null)
 // "null" now all spaces are technically full and will be until the game is completed
 
 //==========================================================================================
+
+// The game will begin once one of the squares is "clicked", after I will add the functionality
+// of adding the value of X or O when clicking on a specific square.
+const launchGame = () => {
+    squares.forEach(square => square.addEventListener('click', spaceClicked));
+}
+
+// Box clicked function:
+function spaceClicked(event) { // upon start og game I want to add an event listener
+// for each square in my boxes array.
+// <- console.log(event.target) <- current event.target to check - when checking this I 
+// can see in the console online that when selecting a square the value it reveals is
+// the coontainer class I created with an id of it's position in the array
+
+}
