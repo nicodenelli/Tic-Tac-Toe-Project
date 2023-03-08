6aa61316b1e7b5ed212c1f52fe5ccae994000009
nicodenelliGH <ndm9412@gmail.com>
2023-03-08 10:53:30 -0500

    Project-1-TicTacToe

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // Base Variables
-let highlightWinner = getComputedStyle(document.body).getPropertyValue('--winningCombo');
-let titleDisplay = document.getElementById('titleDisplay');
-let resetButton = document.getElementById('resetButton');
+let highlightWinner = getComputedStyle(document.body).getPropertyValue('--winningCombo');//<- added at end
 let squares = Array.from(document.getElementsByClassName('square')); // to prevent having to add an event
 // listener to each square I will use the array like function to turn this line into an array
+let titleDisplay = document.getElementById('titleDisplay');
+let resetButton = document.getElementById('resetButton');
 const playerO = "O";
 const playerX = "X";
 let activePlayer = playerO;
 // If a user clicks one of the spaces to place their item 'x' or 'o' that space should now be 
 // unable to be filled with another item as someone has already played that space
 let boxes = Array(9).fill(null)
-// until an item is now placed in one of these squares the space will already be filled with
+// until an item is placed in one of these squares the space will already be filled with
 // "null" now all spaces are technically full and will be until the game is completed
 
 //==========================================================================================
@@ -22,11 +22,11 @@ const launchGame = () => {
 }
 
 // Box clicked function:
-function spaceClicked(event) { // upon start og game I want to add an event listener
+function spaceClicked(event) { // upon start of game I want to add an event listener
 // for each square in my boxes array.
 // <- console.log(event.target) <- current event.target to check - when checking this I 
 // can see in the console online that when selecting a square the value it reveals is
-// the container class I created with an id of it's position in the array
+// the container class I created with an id of it's position in the array (<container class="square" id="0"></container>)
 const id = event.target.id //<- applying the event listener
 if(!boxes[id]) { // <- id from my html - checking to make sure the box selected does not contain this id
     boxes[id] =  activePlayer;
